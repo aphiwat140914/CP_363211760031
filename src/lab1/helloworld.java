462e39f0f3f5ad4e0095b56bab8b98229ae3755d
aphwit <aphiwat.s@rmutsvmail.com>
2020-07-15 14:56:34 +0700

    add more code

--- src/lab1/helloworld.java
+++ src/lab1/helloworld.java
@@ -3,5 +3,10 @@ package lab1;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("helloworid");
+        System.out.println("aphiwat srikhwanchuai");
+        System.out.println("363211760031");
+        System.out.println("เทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+
     }
 }
