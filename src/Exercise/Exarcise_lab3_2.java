e2f1733fae1ae9f6d2ff744baeaa2ff5eb3fe602
aphiwat <aphwat.s@rmutsvmail.com>
2020-08-26 15:47:34 +0700

    อภิวัฒน์ ศรีขวัญช่วย

--- src/Exercise/Exarcise_lab3_2.java
+++ src/Exercise/Exarcise_lab3_2.java
@@ -1,14 +1,15 @@
 package Exercise;
 
-public class Exarsise_Lab3_2 {
+
+public class Exarcise_lab3_2 {
+
     public static void main(String[] args) {
 
-
-        for (int i = 1; i<=10; i++){
-            for (int j = 1; j <= 10; j++){
-                System.out.print("*");
-            }
+        for (int i = 1; i <=10 ; i++) {  // row
+            for (int j = 1; j <= i; j++) { // column
+                System.out.print(j);
+            }//j
             System.out.println();
-        }
+        }//i
     }
 }
