427641203aacfeb6831b45d4bd4293e96e16bb26
aphiwat <aphwat.s@rmutsvmail.com>
2020-10-07 15:34:02 +0700

    lab 8

---
+++ src/lab8/ArrayException.java
@@ -0,0 +1,31 @@
+package lab8;
+
+public class ArrayException {
+
+    public static void main(String[] args) {
+
+        int num[] = { 10, 20, 30, 40, 50};    //index 0-4
+
+        System.out.println(num[0]);
+        try {
+            System.out.println(num[5]);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Index is invaid");
+        }
+
+        System.out.println(num[4]);
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+}//class
